b1e8ea9288d6e4ffbe6a1b139171b270946f3bc3
Deep Lotia <deeplotia@gmail.com>
2020-10-20 14:51:37 +0200

    Update FindBug.java

--- src/main/java/com/d3/prac/interview/rackspace/FindBug.java
+++ src/main/java/com/d3/prac/interview/rackspace/FindBug.java
@@ -3,7 +3,7 @@ package com.d3.prac.interview.rackspace;
 public class FindBug {
 
     int solution(int[] A) {
-        int ans = 0;
+        int ans = A[0];
         for (int i = 1; i < A.length; i++) {
             if (ans > A[i]) {
                 ans = A[i];
